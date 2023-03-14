893326e329d490024128c13ce75496a5425768cf
lucas-alexandrino <lucas.alexcontato@gmail.com>
2023-03-14 18:43:22 -0300

    Blog pessoal

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -12,69 +12,55 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-
 @Entity
 @Table(name = "tb_postagens")
 public class Postagem {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotBlank(message = "O atributo título é obrigatório!")
-	@Size (min = 5 , max = 100)
+	@Size(min = 5, max = 100)
 	private String Titulo;
-	
+
 	@NotBlank(message = "O atributo texto é obrigatório!")
-	@Size (min = 10 , max = 1000, message ="O atributo deve conter no mínimo 10 e no máximo 100 caracteres")
+	@Size(min = 10, max = 1000, message = "O atributo deve conter no mínimo 10 e no máximo 100 caracteres")
 	private String texto;
 
-	
 	@UpdateTimestamp
 	private LocalDateTime data;
-
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getTitulo() {
 		return Titulo;
 	}
 
-
 	public void setTitulo(String titulo) {
 		Titulo = titulo;
 	}
-
 
 	public String getTexto() {
 		return texto;
 	}
 
-
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
-
 
 	public LocalDateTime getData() {
 		return data;
 	}
 
-
 	public void setData(LocalDateTime data) {
 		this.data = data;
 	}
-	
-	
-	
-	
-}
 
+}
